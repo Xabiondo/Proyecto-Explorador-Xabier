5d5c2717ba104ca1655a76b6dcd000eaac3dc8f9
Xabiondo <xgarciaber@educacion.navarra.es>
2025-01-08 11:09:23 +0100

    último commit
    He terminado el proyecto, he arreglado las funcionalidades de moverse, he implmentado la lógica de que al encontrar un enemigo o mina  , se salga del bucle y del juego.

---
+++ Explorador v.3/Explorador/src/Explorador.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Explorador {
+    private String nombre;
+    private Posicion posicion;
+    public static final int ARRIBA = 1;
+    public static final int ABAJO = 2;
+    public static final int DERECHA = 3;
+    public static final int IZQUIERDA = 4;
+
+
+    public Explorador(String queNombre){
+        this.nombre = queNombre;
+        this.posicion = new Posicion((int)(Math.random()*5) , 0);
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Posicion getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(Posicion posicion) {
+        this.posicion = posicion;
+    }
+
+    public void moverse(int direccion) {
+        switch (direccion) {
+            case ARRIBA:
+                if (getPosicion().getCoordenadaFila() > 0){
+                posicion.setCoordenadaFila(posicion.getCoordenadaFila() - 1);
+                }else System.out.println("no puedes ir arriba, te quedas quieto");
+                break;
+            case ABAJO:
+                posicion.setCoordenadaFila(posicion.getCoordenadaFila() + 1);
+                break;
+            case DERECHA:
+                posicion.setCoordenadaCol(posicion.getCoordenadaCol() + 1);
+                break;
+            case IZQUIERDA:
+                posicion.setCoordenadaCol(posicion.getCoordenadaCol() - 1);
+                break;
+
+        }
+
+}}
+
+
